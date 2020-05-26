1252c9099ac8b364d7b714ac987e1a80a304819b
Andrew Durrant <andrew.durrant75@gmail.com>
2020-05-26 17:30:36 -0600

    refactor for database deployment

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 module.exports = {
   NODE_ENV: process.env.NODE_ENV || 'development',
   PORT: process.env.PORT || 8000,
-  DB_URL: process.env.DB_URL || 'postgresql://open_mic@localhost/open-mic',
+  DATABASE_URL: process.env.DATABASE_URL || 'postgresql://open_mic@localhost/open-mic',
+  TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://open_mic@localhost/open-mic',
 };
